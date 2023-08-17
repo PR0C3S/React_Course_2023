31afe512e9a6cd683cafdabda91cbda9aedeac34
JOHN  H. PEÑA <50205406+PR0C3S@users.noreply.github.com>
2023-08-16 21:13:48 -0400

    Accordition Component Exercise v2

--- exercise-accordition-component/src/App.jsx
+++ exercise-accordition-component/src/App.jsx
@@ -25,31 +25,54 @@ export default function App() {
 }
 
 function Accordion({ data }) {
+  const [curOpen, setCurOpen] = useState(null);
+
+  function handleChangCurOpen(number) {}
+
   return (
     <div className="accordion">
       {data.map((item, index) => (
         <AccordionItem
+          curOpen={curOpen}
+          onOpen={setCurOpen}
           key={item.title}
           number={index}
           title={item.title}
-          text={item.text}
-        />
+        >
+          {item.text}
+        </AccordionItem>
       ))}
+      <AccordionItem
+        curOpen={curOpen}
+        onOpen={setCurOpen}
+        title="Test 1"
+        number={22}
+        key="test 1"
+      >
+        <p>Allows React developers to:</p>
+        <ul>
+          <li>Break up UI into components</li>
+          <li>Make components reusuable</li>
+          <li>Place state efficiently</li>
+        </ul>
+      </AccordionItem>
     </div>
   );
 }
 
-function AccordionItem({ number, title, text }) {
-  const [isOpen, setIsOpen] = useState(false);
+function AccordionItem({ curOpen, onOpen, number, title, children }) {
+  const isOpen = number === curOpen;
+
+  function handleToggle() {
+    onOpen(isOpen ? null : number);
+  }
+
   return (
-    <div
-      className={`item ${isOpen ? "open" : ""}`}
-      onClick={() => setIsOpen(!isOpen)}
-    >
+    <div className={`item ${isOpen ? "open" : ""}`} onClick={handleToggle}>
       <p className="number">{number < 9 ? `0${number + 1}` : number + 1}</p>
       <p className="title">{title}</p>
       <p className="icon">{isOpen ? "-" : "+"}</p>
-      {isOpen ? <div className="content-box">{text}</div> : null}
+      {isOpen ? <div className="content-box">{children}</div> : null}
     </div>
   );
 }
