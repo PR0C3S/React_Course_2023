b85572f403c6e2daf74acc55703d53c436379a56
JOHN  H. PEÑA <50205406+PR0C3S@users.noreply.github.com>
2023-08-29 15:10:24 -0400

    Use Pop Corn with out Hooks v0

---
+++ 07-usepopcorn/src/main.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+//import App from './App.jsx'
+import "./App.css";
+
+import StarRating from "./StarRating";
+import App from "./App";
+
+/*function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <div>
+      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+      <p>This movie was rated {movieRating} stars</p>
+    </div>
+  );
+}*/
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    {/*<StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarRating size={24} color="red" className="test" defaultRating={3} />
+    <Test />*/}
+
+    <App />
+  </React.StrictMode>
+);
