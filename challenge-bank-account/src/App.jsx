6c65c280fdce3820078955516c9acef2f26068ed
JOHN  H. PEÑA <50205406+PR0C3S@users.noreply.github.com>
2023-09-17 12:22:27 -0400

    useReducer Bank Account Challenge v1

---
+++ challenge-bank-account/src/App.jsx
@@ -0,0 +1,122 @@
+import { useReducer } from "react";
+
+const initialState = {
+  balance: 0,
+  loan: 0,
+  isActive: false,
+};
+
+function reducer(state, action) {
+  /*
+INSTRUCTIONS / CONSIDERATIONS:
+
+
+
+6. When the customer pays the loan, the opposite happens: the money is taken from the balance, and the 'loan' will get back to 0. This can lead to negative balances, but that's no problem, because the customer can't close their account now (see next point)
+
+7. Customer can only close an account if there is no loan, AND if the balance is zero. If this condition is not met, just return the state. If the condition is met, the account is deactivated and all money is withdrawn. The account basically gets back to the initial state
+*/
+  if (!state.isActive && action.type !== "openAccount") return state;
+
+  switch (action.type) {
+    case "openAccount":
+      return { ...state, isActive: true, balance: 500 };
+
+    case "deposit":
+      return { ...state, balance: state.balance + action.payload };
+    case "withdraw":
+      return { ...state, balance: state.balance - action.payload };
+    case "requestLoan":
+      if (state.loan === 0)
+        return {
+          ...state,
+          loan: action.payload,
+          balance: state.balance + action.payload,
+        };
+      return state;
+    case "payLoan":
+      if (state.loan > 0)
+        return { ...state, balance: state.balance - state.loan, loan: 0 };
+      return state;
+    case "closeAccount":
+      if (state.loan === 0 && state.balance === 0) return initialState;
+      return state;
+    default:
+      throw new Error("Transition unknown");
+  }
+}
+
+export default function App() {
+  const [{ balance, loan, isActive }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+  return (
+    <div className="App">
+      <h1>useReducer Bank Account</h1>
+      <p>Balance: {balance}</p>
+      <p>Loan: {loan}</p>
+
+      <p>
+        <button
+          onClick={() => {
+            dispatch({ type: "openAccount" });
+          }}
+          disabled={isActive}
+        >
+          Open account
+        </button>
+      </p>
+      <p>
+        <button
+          onClick={() => {
+            dispatch({ type: "deposit", payload: 150 });
+          }}
+          disabled={!isActive}
+        >
+          Deposit 150
+        </button>
+      </p>
+      <p>
+        <button
+          onClick={() => {
+            dispatch({ type: "withdraw", payload: 50 });
+          }}
+          disabled={!isActive}
+        >
+          Withdraw 50
+        </button>
+      </p>
+      <p>
+        <button
+          onClick={() => {
+            dispatch({ type: "requestLoan", payload: 5000 });
+          }}
+          disabled={!isActive}
+        >
+          Request a loan of 5000
+        </button>
+      </p>
+      <p>
+        <button
+          onClick={() => {
+            dispatch({ type: "payLoan" });
+          }}
+          disabled={!isActive}
+        >
+          Pay loan
+        </button>
+      </p>
+      <p>
+        <button
+          onClick={() => {
+            dispatch({ type: "closeAccount" });
+          }}
+          disabled={!isActive}
+        >
+          Close account
+        </button>
+      </p>
+    </div>
+  );
+}
